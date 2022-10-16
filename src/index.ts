9d5e0ba4c8416f8d57d3003d004d0c5c62bb6e8f
Brian Steele <brian.steele@hey.com>
2022-10-16 15:20:12 -0700

    some fixes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-const foo = 'bar'
+import express from 'express'
+const app = express()
+
+const PORT = '3000'
+
+app.get('/', (req, res) => {
+  console.log(req)
+  res.json({
+    brian: 'is cool',
+  })
+})
+app.get('/guest/:id', (req, res) => {
+  console.log(req.path)
+  res.json({
+    brian: 'is not cool',
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`)
+})
